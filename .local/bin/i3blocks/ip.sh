0c9b3dfc14bfdb6e3339dbb8987e6e1368a0dc4e
bajok <bajok@kretes.xyz>
2022-04-20 03:24:12 +0200

    fix ovpn i3block

--- .local/bin/i3blocks/ip.sh
+++ .local/bin/i3blocks/ip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ip=$(/usr/bin/ip addr | /usr/bin/grep 'UP' -A2 | /usr/bin/tail -n1 | /usr/bin/awk '{print $2}' | /usr/bin/cut -f1  -d'/')
+ip=$(/usr/bin/ip a | /usr/bin/grep 'UP' -A2 | /usr/bin/grep 'inet\s' | /usr/bin/tail -n1 | /usr/bin/awk '{print $2}' | /usr/bin/cut -f1 -d'/')
 
 [ -n "${BLOCK_BUTTON}" ] && case "${BLOCK_BUTTON}" in
     1) echo "${ip}" | /usr/bin/xclip -selection c && /usr/bin/notify-send "IP address copied" ;; # left click, copy address
